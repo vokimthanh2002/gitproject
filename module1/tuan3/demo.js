d4cc3acdcd5ec42c74f6962765099413675c0e99
vokimthanh2002 <vothanh6081@gmail.com>
2022-06-20 08:59:57 +0700

    updata tuan3

---
+++ module1/tuan3/demo.js
@@ -0,0 +1,20 @@
+
+function showMessage(){
+    alert('Xin chào!');
+}
+let i= 10
+let f=20.5
+let b= true
+let s= "Ha noi"
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+document.write('<br/>')
+let width = 20
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
